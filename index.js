f1623cf9be2e44e596417c6a301fa8b94189794c
queicherius <queicherius@gmail.com>
2016-06-21 01:15:18 +0200

    Fix language detection for loading

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var converter = require('i18next-conv-inline')
 module.exports = function (source) {
   this.cacheable && this.cacheable()
   var callback = this.async()
+  let language = source.match(/Language: ([\w]*)/)
+  language = language ? language[1] : 'en'
 
-  converter.gettextToI18next('en', source, {quiet: true}, function (err, data) {
+  converter.gettextToI18next(language, source, {quiet: true}, function (err, data) {
     if (err) {
       return callback(err)
     }

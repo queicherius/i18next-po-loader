68e10f6f44319b5de543b905f57fca70eb8081ec
queicherius <queicherius@gmail.com>
2016-06-24 18:15:22 +0200

    Update dependency, ignore untranslated keys

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-var converter = require('i18next-conv-inline')
+var converter = require('i18next-conv')
+var options = {
+  skipUntranslated: true,
+  keyseparator: '°#°#°#°#°'
+}
 
 module.exports = function (source) {
   this.cacheable && this.cacheable()
   var callback = this.async()
-  let language = source.match(/Language: ([\w]*)/)
+
+  // Parse the language out of the file
+  var language = source.match(/Language: ([\w]*)/)
   language = language ? language[1] : 'en'
 
-  converter.gettextToI18next(language, source, {quiet: true}, function (err, data) {
-    if (err) {
-      return callback(err)
-    }
-
-    callback(null, "module.exports = " + JSON.stringify(data, undefined, "\t") + ";")
+  // Convert to i18next format (already stringified)
+  converter.gettextToI18next(language, source, options).then(function (data) {
+    callback(null, "module.exports = " + data + ";")
   })
 }

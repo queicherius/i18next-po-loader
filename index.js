37caa8677f52491dab8461f1483780f1a7a16749
Raffaele Canale <raffaelecanale@gmail.com>
2017-09-27 15:53:40 +0200

    Default missing keys with root language

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 var converter = require('i18next-conv')
-var options = {
-  skipUntranslated: true,
-  keyseparator: '°#°#°#°#°'
+var utils = require('loader-utils')
+var _ = require('lodash')
+var fs = require('fs')
+
+var defaultOptions = {
+    skipUntranslated: true,
+    keyseparator: '°#°#°#°#°',
+    rootLanguage: './src/js/nls/root/strings.po',
+}
+
+function loadRoot(options) {
+    return fs.readFileSync(options.rootLanguage, "utf-8")
+}
+
+function parse(source, options) {
+    var language = source.match(/Language: ([\w]*)/)
+    language = language ? language[1] : 'en'
+
+    // Convert to i18next format (already stringified)
+    return converter.gettextToI18next(language, source, options)
 }
 
 module.exports = function (source) {
-  this.cacheable && this.cacheable()
-  var callback = this.async()
+    this.cacheable && this.cacheable()
+    var callback = this.async()
+    var options = _.defaults(utils.getOptions(this), defaultOptions)
 
-  // Parse the language out of the file
-  var language = source.match(/Language: ([\w]*)/)
-  language = language ? language[1] : 'en'
+    Promise.all([parse(source, options), parse(loadRoot(options), options)])
+        .then((bundles) => {
+            var language = JSON.parse(bundles[0])
+            var root = JSON.parse(bundles[1])
+            var merged = _.defaults({}, language, root)
 
-  // Convert to i18next format (already stringified)
-  converter.gettextToI18next(language, source, options).then(function (data) {
-    callback(null, "module.exports = " + data + ";")
-  })
+            callback(null, 'module.exports = ' + JSON.stringify(merged) + ';')
+        })
 }
